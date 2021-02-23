bc6ca543f1ea42619dedacbfdd1dde7633a342ce
adj006 <ajim1989@gmail.com>
2021-02-22 16:40:46 -0800

    Implemented handling POST request with error handling

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -12,4 +12,21 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password = request.form.get('password')
+        confirmPassword = request.form.get('confirmPassword')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than a character.', category='error')
+        elif password != confirmPassword:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password) < 8:
+            flash('Password must be at least 8 characters.', category='error')
+        else:
+            # Add user to database
+            flash('Account created!.', category='success')
     return render_template("sign_up.html")
